ea687a674c9b91a4536fb5dee44f6f71b7b682a9
Calandra-a <acalandra@oakland.edu>
2018-10-30 12:08:02 -0400

    updates to measurement model

--- mcl.cpp
+++ mcl.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <math>
 using namespace std;
 
 class particle{
@@ -33,9 +34,34 @@ class control{
 	}
 
 };
+class feature{
+	int range;
+	int bearing; //degrees?
+	int signiture; //signiture 
+	int correspondence;
 
-class ccupancyGridMap{
-	
+	void create(int r,b,s,c){
+		range = r;
+		bearing = b;
+		signiture = s;
+		correspondence = c;
+	}
+}
+
+class map{
+	vector<feature> map;	
+
+	map(){
+		populateMap(3);
+	}
+	static void populateMap(int numOfFeatures){
+
+		for(int i =0; i< numOfFeatures; i++){
+			feature f;
+			f.create(i,i,i,i);
+		}
+	}
+
 }
 
 int main(){
@@ -46,16 +72,18 @@ int main(){
 vector<particle> mcl(vector<particle> inParticles ,control movement, int sampleSize){
 	vector<particle> predSample;
 	std::vector<particle> resample;
+	map Map;
+	control Movement;
 	//Xbar = X = NUll
 	predSample.clear();
 	resample.clear();
 	//predicitive sampling
 	particle p;
 	for(int i =0; i < sampleSize; i++){
-		motion_model(p, inParticles.at(i),control movement);
+		motion_model(p, inParticles.at(i),Movement);
 
-		//p.setWeight = MeasurmentModel
-		//predSample.add(p);
+		p.setWeight = MeasurmentModel(p, Map.map.at(i))
+		predSample.add(p);
 	}
 	particle prs;//particle resampled
 	for(int i =0; i < sampleSize; i++){
@@ -69,7 +97,7 @@ vector<particle> mcl(vector<particle> inParticles ,control movement, int sampleS
 //positive rotation is left
 //positive translation is forward 
 void motion_model(particle new, particle previous, control move){ 
-
+ 
 //int update[3]; //updated pose x,y,theta
 int xCenter;
 int yCenter;
@@ -82,21 +110,26 @@ new.pose[1] = yCenter - (move.Tvel/move.Rvel)*cos(previous.pose[2]+ (move.Rvel*m
 new.pose[2] = move.Rvel*move.duration;
 }
 
-void MeasurmentModel(control move, particle current, map){//occupancy grid map???
-
-	int q = 1;
-
-	for (int i = 0; i < I; i++){
-
-
-	}
+int  MeasurmentModel(feature feature,particle p){//occupancy grid map???
+		j=feature.correspondence
+		int tRange; // r-hat
+		tRange = sqrt(((map.getX(j)-p.pose[0])*(map.getX(i)-p.pose[0]))+((map.getY(j)-p.pose[1])*(map.getY(j)-p.pose[1])))
+		int rBearing; //Phi-hat
+		tBearing = atan2((map.getY(j)-p.pose[1]),(map.getX(j)-p.pose[0]));
+		int q; //numerical probablity p(f[i] at time t | c[i] at time t, m, x at time t)
+		q = prob(feature.range - tRange,StandardDevR) * prob(feature.bearing - tBearing, StandardDevB) * prob(feature.signiture - signiture(j), StandardDevS); 
+		return q; 
 }
 
 
+
+
 /*TODO
--Occupancy Grid Map
--Measurment Model
-	weight attribute in particle
+feature extractor to generate
+-map - Vector of features
+-features - to populate map
+-establish releated correspondence to each feature
+
 -draw i with prob proportional with w[i]
 -write main
 */
